3c32c75887e19ecb9cf9adf248735918e41c9828
XIOMY-SKAYURI <xiomygarciaguerrero@gmail.com>
2020-08-24 10:45:21 -0500

    avance1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-import { example } from './data.js';
+import { lol } from './data.js';
 import data from './data/lol/lol.js';
 // import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(lol, data);
+const logo = document.getElementById("logo");
+const userName = document.getElementById("userName");
+const screen2 = document.getElementById("screen2");
+const play = document.getElementById("play");
+//Para ocultar la primera pantalla
+play.addEventListener("click", () => {
+    logo.classList.add("ocultar");
+    play.classList.add("ocultar");
+    userName.classList.add("ocultar");
+    screen2.classList.remove("ocultar");
+})
+
+let letsPlay = document.getElementById("play");
+letsPlay.addEventListener("click", () => {
+    let names = document.getElementById("userName").value;
+    let userNames = names;
+    document.getElementById("showName").innerHTML = userNames;
+    return userNames
+})
