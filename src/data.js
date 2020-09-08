2bf78b902828e6c552e33c5be59e607d082e6617
XIOMY-SKAYURI <xiomygarciaguerrero@gmail.com>
2020-09-08 17:15:02 -0500

    nuevos cambios en filtrado y orden de campeones

--- src/data.js
+++ src/data.js
@@ -3,26 +3,143 @@
 
  import lol from "./data/lol/lol.js";
  const arrayChampions = Object.values(lol.data);
+ console.log(arrayChampions);
 
+ const resultAllAssassin = arrayChampions.filter((objetoCampeon) => {
+     return objetoCampeon.tags.includes("Assassin");
+ });
+ console.log(resultAllAssassin);
 
- export const filter = {
+ const resultAllFighter = arrayChampions.filter((objetoCampeon) => {
+     return objetoCampeon.tags.includes("Fighter");
+ });
+ console.log(resultAllFighter);
 
-     type: function() {
-         const resultLol = arrayChampions.filter((objetoCampeon) => {
-             return objetoCampeon.tags.includes("Assassin");
-         });
-         console.log(resultLol);
-         return resultLol;
+ const resultAllMage = arrayChampions.filter((objetoCampeon) => {
+     return objetoCampeon.tags.includes("Mage");
+ });
+ //console.log(resultAllMage);
+
+ const resultAllMarksman = arrayChampions.filter((objetoCampeon) => {
+     return objetoCampeon.tags.includes("Marksman");
+ });
+ //console.log(resultAllMarksman);
+
+ const resultAllTank = arrayChampions.filter((objetoCampeon) => {
+     return objetoCampeon.tags.includes("Tank");
+ });
+ //console.log(resultAllTank);
+
+ const resultAllSupport = arrayChampions.filter((objetoCampeon) => {
+     return objetoCampeon.tags.includes("Support");
+ });
+ //console.log(resultAllSupport);
+
+ const ordenaZaA = arrayChampions.sort((a, b) => {
+     if (a.name < b.name) {
+         return 1;
      }
- };
+     if (a.name > b.name) {
+         return -1;
+     }
+     return 0;
+ });
+ console.log(ordenaZaA);
+
+ export { lol, resultAllAssassin, resultAllFighter, resultAllMage, resultAllMarksman, resultAllTank, resultAllSupport, ordenaZaA }
+
+ /*
+  
+  for (let i in resultAllTank) {
+      console.log(` ${resultAllTank [i].name}`);
+      //console.log(resultLol[i].name);
+      console.log(` ${resultAllTank [i].splash}`);
+      //console.log(resultLol[i].name);
+      console.log(` Attack: ${resultAllTank [i].info.attack}`);
+      console.log(` Defense: ${resultAllTank [i].info.defense}`);
+      console.log(` Magic: ${resultAllTank [i].info.magic}`);
+      console.log(` Difficulty: ${resultAllTank [i].info.difficulty}`);
 
 
- //console.log(filterAssassin);
+      const resultAllMage = arrayChampions.filter((objetoCampeon) => {
+          return objetoCampeon.tags.includes("Mage");
+      });
+      console.log(resultAllMage);
+      for (let i in resultAllMage) {
+          console.log(` ${resultAllMage[i].name}`);
+          //console.log(resultLol[i].name);
+          console.log(` ${resultAllMage[i].splash}`);
+          //console.log(resultLol[i].name);
+          console.log(` Attack: ${resultAllMage[i].info.attack}`);
+          console.log(` Defense: ${resultAllMage[i].info.defense}`);
+          console.log(` Magic: ${resultAllMage[i].info.magic}`);
+          console.log(` Difficulty: ${resultAllMage[i].info.difficulty}`);
+
+          const resultAllFighter = arrayChampions.filter((objetoCampeon) => {
+              return objetoCampeon.tags.includes("Fighter");
+          });
+          console.log(resultAllFighter);
+          for (let i in resultAllFighter) {
+              console.log(` ${resultAllFighter[i].name}`);
+              //console.log(resultLol[i].name);
+              console.log(` ${resultAllFighter[i].splash}`);
+              //console.log(resultLol[i].name);
+              console.log(` Attack: ${resultAllFighter[i].info.attack}`);
+              console.log(` Defense: ${resultAllFighter[i].info.defense}`);
+              console.log(` Magic: ${resultAllFighter[i].info.magic}`);
+              console.log(` Difficulty: ${resultAllFighter[i].info.difficulty}`);
+
+              const resultAllMarksman = arrayChampions.filter((objetoCampeon) => {
+                  return objetoCampeon.tags.includes("Marksman");
+              });
+              console.log(resultAllMarksman);
+              for (let i in resultAllMarksman) {
+                  console.log(` ${resultAllMarksman[i].name}`);
+                  //console.log(resultLol[i].name);
+                  console.log(` ${resultAllMarksman[i].splash}`);
+                  //console.log(resultLol[i].name);
+                  console.log(` Attack: ${resultAllMarksman[i].info.attack}`);
+                  console.log(` Defense: ${resultAllMarksman[i].info.defense}`);
+                  console.log(` Magic: ${resultAllMarksman[i].info.magic}`);
+                  console.log(` Difficulty: ${resultAllMarksman[i].info.difficulty}`);
+
+                  const resultAllSupport = arrayChampions.filter((objetoCampeon) => {
+                      return objetoCampeon.tags.includes("Support");
+                  });
+                  console.log(resultAllSupport);
+                  for (let i in resultAllSupport) {
+                      console.log(` ${resultAllSupport[i].name}`);
+                      //console.log(resultLol[i].name);
+                      console.log(` ${resultAllSupport[i].splash}`);
+                      //console.log(resultLol[i].name);
+                      console.log(` Attack: ${resultAllSupport[i].info.attack}`);
+                      console.log(` Defense: ${resultAllSupport[i].info.defense}`);
+                      console.log(` Magic: ${resultAllSupport[i].info.magic}`);
+                      console.log(` Difficulty: ${resultAllSupport[i].info.difficulty}`);
 
 
 
 
 
- export const anotherExample = () => {
-     return 'OMG';
- };
+
+
+
+
+
+                      export const filter = {
+
+                      type: function() {
+
+                      }
+                  };
+
+
+                  //console.log(filterAssassin);
+
+
+
+
+
+                  export const anotherExample = () => {
+                      return 'OMG';
+                  };*/
--- src/main.js
+++ src/main.js
@@ -1,141 +1,220 @@
-import { filter } from './data.js';
-
+import { resultAllAssassin } from './data.js';
+import { resultAllFighter } from './data.js';
+import { resultAllMage } from './data.js';
+import { resultAllMarksman } from './data.js';
+import { resultAllTank } from './data.js';
+import { resultAllSupport } from './data.js';
+import { ordenaZaA } from './data.js';
 import data from './data/lol/lol.js';
+console.log(data);
 
+const buttonAssassin = document.getElementById("assassin");
+const buttonFighter = document.getElementById("fighter");
+const showChampions = document.getElementById("showChampions");
+const showAssassin = document.getElementById("showAssassin");
+const showFighter = document.getElementById("showFighter");
 
-
-
-/*const LOLData = data.lol;
-
-
-console.log('cantidad:' + LOLData.length);*/
-//function mostrar todos los campeones
-
+//MOSTRAR TODOS LOS CAMPEONES
+console.log("hola");
 for (const [key, value] of Object.entries(data.data)) {
     let id = value.id;
-    console.log(id);
-    let img = value.img;
-    console.log(img);
+    let name = id.toUpperCase();
+    let img = value.splash;
     let infoAttack = value.info.attack;
-    console.log(infoAttack);
     let infoDefense = value.info.defense;
-    console.log(infoDefense);
     let infoMagic = value.info.magic;
-    console.log(infoMagic);
-    let infoDifficulty = value.info.difficulty;
-    console.log(infoDifficulty);
-
 
     const infoChampions = document.getElementById("information");
     const containerHero = document.createElement("div");
     containerHero.className = "containerHero";
     const nameHero = document.createElement("p");
-    nameHero.innerHTML = id
+    nameHero.innerHTML = name
     nameHero.className = "nameChampions";
     containerHero.appendChild(nameHero);
 
     const newDivImg = document.createElement("img");
     newDivImg.setAttribute("src", img);
-    newDivImg.setAttribute("width", "150");
+    newDivImg.setAttribute("width", "200");
     newDivImg.setAttribute("height", "150");
-    containerHero.appendChild(newDivImg);
+    containerHero.appendChild(newDivImg)
+
+    const containerInfo = document.createElement("div");
+    containerInfo.className = "containerInfo";
 
     const newDivAttack = document.createElement("p");
-    newDivAttack.innerHTML = "<span style = 'color:green; font-weight: bolder;'>  Attack: </span>" + infoAttack;
-    newDivAttack.className = "aboutInfo";
-    containerHero.appendChild(newDivAttack);
+    newDivAttack.innerHTML = "<span style = 'color:white; font-weight: bolder;'> Attack: </span>" + infoAttack;
+    // newDivAttack.className = "aboutInfo";
+    containerInfo.appendChild(newDivAttack);
+    containerHero.appendChild(containerInfo);
 
     const newDivDefense = document.createElement("p");
-    newDivDefense.innerHTML = "<span style = 'color:green; font-weight: bolder;'> Defense: </span>" + infoDefense;
-    newDivAttack.className = "aboutInfo";
-    containerHero.appendChild(newDivDefense);
+    newDivDefense.innerHTML = "<span style = 'color:white; font-weight: bolder;'> Defense: </span>" + infoDefense;
+    //newDivAttack.className = "aboutInfo";
+    containerInfo.appendChild(newDivDefense)
+    containerHero.appendChild(containerInfo);
 
     const newDivMagic = document.createElement("p");
-    newDivMagic.innerHTML = "<span style = 'color:green;font-weight: bolder;'> Magic: </span>" + infoMagic;
-    newDivAttack.className = "aboutInfo";
-    containerHero.appendChild(newDivMagic);
-
-    const newDivDifficulty = document.createElement("p");
-    newDivDifficulty.innerHTML = "<span style = 'color:green;font-weight: bolder;'> Difficulty: </span>" + infoDifficulty;
-    newDivAttack.className = "aboutInfo";
-    containerHero.appendChild(newDivDifficulty);
-
-
+    newDivMagic.innerHTML = "<span style = 'color:white;font-weight: bolder;'> Magic: </span>" + infoMagic;
+    //newDivAttack.className = "aboutInfo";
+    containerInfo.appendChild(newDivMagic);
+    containerHero.appendChild(containerInfo);
     infoChampions.appendChild(containerHero);
-    /*const newDivName = document.createElement("p");
-    newDivName.innerHTML = id;
-    document.getElementById("AllChampions").appendChild(newDivName);*/
-
-    // document.querySelector("#AllInfoChampions").innerHTML += info
-
 };
 
-function typeFilter(event) {
-    console.log(event.target.id)
-}
 
-// querySelectorAll arreglo con los elmentos coincidad 
-let btnFilter = document.querySelectorAll('.btnFilter')
-
-// recorremos el arreglo para asignar el evento
-for (let i = 0; i < btnFilter.length; i++) {
-    btnFilter[i].addEventListener('click', typeFilter)
-}
-
-/*const newDivInfo = document.createElement("text");
-newDivName.innerHTML = info;
-document.getElementById("AllInfoChampions").appendChild(newDivInfo);*/
+//MOSTRAR TODOS LOS ASSESINOS
+//Type Assassin
+for (const [key, value] of Object.entries(resultAllAssassin)) {
+    let id = value.id;
+    let name = id.toUpperCase()
+    let img = value.splash;
+    let infoAblurb = value.blurb;
 
 
+    const infoAssassin = document.getElementById("filterAssassin");
+    const containerAssassin = document.createElement("div");
+    containerAssassin.className = "containerAssassin";
+    const nameAssassins = document.createElement("p");
+    nameAssassins.innerHTML = name;
+    nameAssassins.className = "namesChampions";
+    containerAssassin.appendChild(nameAssassins);
+
+    const ImgAssassin = document.createElement("img");
+    ImgAssassin.setAttribute("src", img);
+    ImgAssassin.setAttribute("width", "200");
+    ImgAssassin.setAttribute("height", "150");
+    containerAssassin.appendChild(ImgAssassin);
+
+    const newDivBlurb = document.createElement("p");
+    newDivBlurb.innerHTML = infoAblurb;
+    newDivBlurb.className = "aboutInfo";
+    containerAssassin.appendChild(newDivBlurb);
+
+    const containerButtons = document.createElement("div");
+    containerButtons.className = "containerButtons";
+    const buttonInfoGeneral = document.createElement("button");
+    buttonInfoGeneral.innerHTML = "Information";
+    buttonInfoGeneral.className = " buttonInfoGeneral";
+    const buttonStats = document.createElement("button");
+    buttonStats.innerHTML = "Stats";
+    buttonStats.className = "buttonStats";
+    containerButtons.appendChild(buttonInfoGeneral);
+    containerButtons.appendChild(buttonStats);
+    containerAssassin.appendChild(containerButtons);
+
+    infoAssassin.appendChild(containerAssassin);
+};
+
+buttonAssassin.addEventListener("click", () => {
+    showAssassin.classList.remove("ocultar");
+    showChampions.classList.add("ocultar");
+    showFighter.classList.add("ocultar");
+
+});
+
+
+/*//Type Fighter
+for (const [key, value] of Object.entries(resultAllFighter)) {
+    let id = value.id;
+    let name = id.toUpperCase()
+    let img = value.splash;
+    let infoAblurb = value.blurb;
+
+    const infoFighter = document.getElementById("filterFighter");
+    const containerFighter = document.createElement("div");
+    containerFighter.className = "containerFighter";
+    const nameFighter = document.createElement("p");
+    nameFighter.innerHTML = name;
+    nameFighter.className = "namesChampions";
+    containerFighter.appendChild(nameFighter);
+
+    const ImgFighter = document.createElement("img");
+    ImgFighter.setAttribute("src", img);
+    ImgFighter.setAttribute("width", "200");
+    ImgFighter.setAttribute("height", "150");
+    containerFighter.appendChild(ImgFighter);
+
+    const newDivBlurb = document.createElement("p");
+    newDivBlurb.innerHTML = infoAblurb;
+    newDivBlurb.className = "aboutInfo";
+    containerFighter.appendChild(newDivBlurb);
+
+    const containerButtons = document.createElement("div");
+    containerButtons.className = "containerButtons";
+    const buttonInfoGeneral = document.createElement("button");
+    buttonInfoGeneral.innerHTML = "Information";
+    buttonInfoGeneral.className = " buttonInfoGeneral";
+    const buttonStats = document.createElement("button");
+    buttonStats.innerHTML = "Stats";
+    buttonStats.className = "buttonStats";
+    containerButtons.appendChild(buttonInfoGeneral);
+    containerButtons.appendChild(buttonStats);
+    containerFighter.appendChild(containerButtons);
+    infoFighter.appendChild(containerFighter);
+};
+
+buttonFighter.addEventListener("click", () => {
+    showAssassin.classList.add("ocultar");
+    showFighter.classList.remove("ocultar");
+    showChampions.classList.add("ocultar");
+});
 
 
 
+for (const [key, value] of Object.entries(ordenaZaA)) {
+    let id = value.id;
+    //console.log(id);
+    let img = value.splash;
+    //console.log(img);
+    let infoAttackOrder = value.info.attack;
+    // console.log(infoAttack);
+    let infoDefenseOrder = value.info.defense;
+    //console.log(infoDefense);
+    let infoMagicOrder = value.info.magic;
+    // console.log(infoMagic);
+    let infoDifficultyOrder = value.info.difficulty;
+    // console.log(infoDifficulty);
 
+    const infoOrder = document.getElementById("OrdenarZaZ");
+    const containerOrder = document.createElement("div");
+    containerOrder.className = "containerOrder";
+    const nameOrder = document.createElement("p");
+    nameOrder.innerHTML = id
+    nameOrder.className = "namesChampions";
+    containerOrder.appendChild(nameOrder);
 
+    const ImgOrder = document.createElement("img");
+    ImgOrder.setAttribute("src", img);
+    ImgOrder.setAttribute("width", "200");
+    ImgOrder.setAttribute("height", "150");
+    containerOrder.appendChild(ImgOrder);
 
-/*for (let value of Object.values(data)) {
-    console.log(data.data);
+    const newDivAttackOrder = document.createElement("p");
+    newDivAttackOrder.innerHTML = "<span style = 'color:chocolate; font-weight: bolder;'>  Attack: </span>" + infoAttackOrder;
+    newDivAttackOrder.className = "aboutInfo";
+    containerOrder.appendChild(newDivAttackOrder);
 
-    for (let i = 0; i < 7; i++) {
-        let id = data.data[i].id;
-        console.log(id);
-    }
+    const newDivDefenseOrder = document.createElement("p");
+    newDivDefenseOrder.innerHTML = "<span style = 'color:chocolate; font-weight: bolder;'> Defense: </span>" + infoDefenseOrder;
+    newDivDefenseOrder.className = "aboutInfo";
+    containerOrder.appendChild(newDivDefenseOrder);
 
-}*/
+    const newDivMagicOrder = document.createElement("p");
+    newDivMagicOrder.innerHTML = "<span style = 'color:chocolate;font-weight: bolder;'> Magic: </span>" + infoMagicOrder;
+    newDivMagicOrder.className = "aboutInfo";
+    containerOrder.appendChild(newDivMagicOrder);
 
+    const newDivDifficultyOrder = document.createElement("p");
+    newDivDifficultyOrder.innerHTML = "<span style = 'color:chocolate;font-weight: bolder;'> Difficulty: </span>" + infoDifficultyOrder;
+    newDivDifficultyOrder.className = "aboutInfo";
+    containerOrder.appendChild(newDivDifficultyOrder);
 
-
-
-//let newData = data.lol.type;
-
-//console.log(newData);
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-/*for (var [key, value] of Object.entries(data.data)) {
-    console.log(key + value.id + value.img + value.tags + value);}*/
-
-//console.log(data.data.length);
-
-
-/*const rectangle = document.getElementById("rectangle");
-const nameLogo1 = document.getElementById("nameLogo1");
-//Para ocultar la primera pantalla
-play.addEventListener("click", () => {
-    // alert('Click')
-    logo.classList.add("ocultar");
-    play.classList.add("ocultar");
-    userName.classList.add("ocultar");
-    hello.classList.remove("ocultar");
-    showName.classList.remove("ocultar");
-
-    nameLogo1.classList.remove("ocultar");
-
-})
-
-let letsPlay = document.getElementById("play");
-letsPlay.addEventListener("click", () => {
-    let names = document.getElementById("userName").value;
-    let userNames = names;
-    document.getElementById("showName").innerHTML = userNames;
-    return userNames
+    infoOrder.appendChild(containerOrder);
+};
+const infoChampions = document.getElementById("information")
+const infoOrder = document.getElementById("OrdenarZaZ")
+const OrdenarZaA = document.getElementById("ZaA");
+OrdenarZaA.addEventListener("click", () => {
+    infoChampions.classList.add("ocultar");
+    infoOrder.classList.remove("ocultar");
 });*/
